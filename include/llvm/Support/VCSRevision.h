ed15c6268f72ab666c79b096dde426d6cb071cee
xiangfeidexiaohuo <jsjson@163.com>
2021-03-01 14:35:38 +0800

    AE86-20210301 clang version 13.0.0
    Signed-off-by: xiangfeidexiaohuo <jsjson@163.com>

--- include/llvm/Support/VCSRevision.h
+++ include/llvm/Support/VCSRevision.h
@@ -1,2 +1,2 @@
-#define LLVM_REVISION "0ad86f879f3a999bf0aad19acf15375482a17e99"
+#define LLVM_REVISION "d49270b087e85191057925ce00c19378caeee5b2"
 #define LLVM_REPOSITORY "https://github.com/llvm/llvm-project"
