65ee708f3ccc895cde58aef3e387c7cafbc67834
xiangfeidexiaohuo <jsjson@163.com>
2021-03-31 16:21:50 +0800

    Clang v13.0.0-20210331
    Signed-off-by: xiangfeidexiaohuo <jsjson@163.com>

--- include/llvm/Support/VCSRevision.h
+++ include/llvm/Support/VCSRevision.h
@@ -1,2 +1,2 @@
-#define LLVM_REVISION "58c62fd9768594ec8dd57e8320ba2396bf8b87e5"
+#define LLVM_REVISION "4e0045cc9b00ed18a7b8981d468a9d01eb4f6780"
 #define LLVM_REPOSITORY "https://github.com/llvm/llvm-project"
